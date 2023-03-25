8cee0ac5c60cc999314dfd45763bcf9a61f919ce
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-03-25 01:06:45 -0300

    configuração de packages e nuxt config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
-import { resolve } from 'path'
-
 export default defineNuxtConfig({
-  alias: {
-    "@": resolve(__dirname, "/")
+  app:{
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR'
+      },
+      title: "Template Started",
+      meta: [
+        { name: "viewport",},
+        { content: "width=device-width, initial-scale=1"},
+        { charset: "utf-8"},
+        { name: 'description', content: 'description' }
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap' },
+      ],
+    }    
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: `
+          //   @import '~/assets/styles/reset.scss';
+          //   @import '~/assets/styles/variables.scss';
+          //   @import '~/assets/styles/app.scss';
+          //   `,
+        }
+      }
+    },
   },
   css: ["~/assets/main.scss",],
   postcss: {
